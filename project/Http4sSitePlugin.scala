54ee82bb4ea37a8b9aa102350d6eb1e05d7d51dd
Andrew Valencik <andrew.valencik@shopify.com>
2025-03-16 18:00:00 -0400

    Revert landingPage if condition change

--- project/Http4sSitePlugin.scala
+++ project/Http4sSitePlugin.scala
@@ -51,7 +51,7 @@ object Http4sSitePlugin extends AutoPlugin {
 
       val latest = Http4sPlugin.latestPerMinorVersion(baseDirectory.value)
       // helpful to render landing page when previewing locally
-      if (true)
+      if (version.value.startsWith("1.") || !isCi.value)
         landingPage.configure(
           base,
           latest((0, 23)).toString,

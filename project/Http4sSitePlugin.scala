f2c4f81c4869f3100c7e3ceb35541ce8da9a77f1
Andrew Valencik <andrew.valencik@shopify.com>
2025-03-16 16:46:31 -0400

    Reapply "HACK Don't use protosearch sbt, use laikaIO lib"
    This reverts commit f17e3692206271b74eff9eca909b22d574c82de1.

--- project/Http4sSitePlugin.scala
+++ project/Http4sSitePlugin.scala
@@ -16,6 +16,9 @@ import mdoc.MdocPlugin.autoImport._
 import org.typelevel.sbt.TypelevelSitePlugin.autoImport._
 import org.typelevel.sbt.site.GenericSiteSettings
 import Http4sPlugin.autoImport._
+import pink.cozydev.protosearch.analysis.IndexRendererConfig
+import pink.cozydev.protosearch.ui.SearchUI
+import laika.sbt.LaikaPlugin.autoImport._
 
 object Http4sSitePlugin extends AutoPlugin {
 
@@ -33,9 +36,12 @@ object Http4sSitePlugin extends AutoPlugin {
         s"VERSION_${major}_${minor}" -> v.toString
       }
     },
+    laikaRenderers += IndexRendererConfig(includeInSite = true),
     tlSiteHelium := {
       val base = tlSiteHelium.value
         .extendWith(redirects.theme)
+        .extendWith(SearchUI)
+        .extendWith(SearchUI.searchNavBar(_))
         .site
         .internalCSS(Root / "styles")
         .site
